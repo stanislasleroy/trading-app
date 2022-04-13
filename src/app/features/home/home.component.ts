a52b1fd913ba76aa707be96f5aa07fbfb1cbeb4c
Stanislas LEROY <stanislas.leroy@sis-id.com>
2022-04-13 22:23:19 +0200

    Clean code

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,16 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import {CartService} from '../../shared/services/cart.service';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
-  constructor(private cartService: CartService) { }
-
-  ngOnInit(): void {
-
+  constructor() {
   }
 }
